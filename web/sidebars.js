1da8bc903da2985ef2122d1251528d26da664747
vcarrier <vincent.carrier@cegepmontpetit.ca>
2026-09-20 22:08:01 -0400

    Réorganisation et r2.1

--- web/sidebars.js
+++ web/sidebars.js
@@ -17,195 +17,195 @@ const sidebars = {
           "Mathieu":     [{ "1040": "2026-08-26" }, 
                           { "1050": "2026-08-26" }]
         }, avancementLabel: "TP1", avancement: 0.05 }},
-    { type: "doc", id: "cours/r2.1", customProps: { calendrier: {
+    { type: "doc", id: "cours/r1.3", customProps: { calendrier: {
           "Jean-Michel": [{"1010": "2026-09-01"},
                           {"1020": "2026-09-01"}],
           "Vincent":     [{"1030": "2026-09-01"}],
           "Mathieu":     [{"1040": "2026-08-31"},
                           {"1050": "2026-08-31"}],
         }, avancementLabel: "TP1", avancement: 0.25 }},
-    { type: "doc", id: "cours/r2.2", customProps: { calendrier: {
+    { type: "doc", id: "cours/r1.4", customProps: { calendrier: {
           "Jean-Michel": [{"1010": "2026-09-04"},
                           {"1020": "2026-09-04"}],
           "Vincent":     [{"1030": "2026-09-04"}],
           "Mathieu":     [{"1040": "2026-09-02"},
                           {"1050": "2026-09-02"}],
         }, avancementLabel: "TP1", avancement: 0.50 }},
-    { type: "doc", id: "cours/r3.1", customProps: { calendrier: {
+    { type: "doc", id: "cours/r1.5", customProps: { calendrier: {
           "Jean-Michel": [{"1010": "2026-09-08"},
                           {"1020": "2026-09-08"}],
           "Vincent":     [{"1030": "2026-09-11"}],
           "Mathieu":     [{"1040": "2026-09-09"},
                           {"1050": "2026-09-09"}],
         }, avancementLabel: "TP1", avancement: 0.75 }},
-    { type: "doc", id: "cours/r3.2", customProps: { calendrier: {
+    { type: "doc", id: "cours/r1.6", customProps: { calendrier: {
           "Jean-Michel": [{"1010": "2026-09-11"},
                           {"1020": "2026-09-11"}],
           "Vincent":     [{"1030": "2026-09-15"}],
           "Mathieu":     [{"1040": "2026-09-14"},
                           {"1050": "2026-09-14"}],
         }, avancementLabel: "TP1", avancement: 1.00 }},
-    { type: "doc", id: "cours/r4.1", customProps: { calendrier: {
+    { type: "doc", id: "cours/r1.7", customProps: { calendrier: {
           "Jean-Michel": [{"1010": "2026-09-15"},
                           {"1020": "2026-09-15"}],
           "Vincent":     [{"1030": "2026-09-18"}],
           "Mathieu":     [{"1040": "2026-09-16"},
                           {"1050": "2026-09-16"}],
         }  }},
-    { type: "doc", id: "cours/r4.2", customProps: { calendrier: {
+    { type: "doc", id: "cours/r1.8", customProps: { calendrier: {
           "Jean-Michel": [{"1010": "2026-09-18"},
                           {"1020": "2026-09-18"}],
           "Vincent":     [{"1030": "2026-09-22"}],
           "Mathieu":     [{"1040": "2026-09-21"},
                           {"1050": "2026-09-21"}],
         }  }},
-    { type: "doc", id: "cours/r5.1", customProps: { calendrier: {
+    { type: "doc", id: "cours/r2.1", customProps: { calendrier: {
           "Jean-Michel": [{"1010": "2026-09-22"},
                           {"1020": "2026-09-22"}],
           "Vincent":     [{"1030": "2026-09-25"}],
           "Mathieu":     [{"1040": "2026-09-23"},
                           {"1050": "2026-09-23"}],
-        }, avancementLabel: "TP2", avancement: 0.10 }},
-    { type: "doc", id: "cours/r5.2", customProps: { calendrier: {
+        },  }},
+    { type: "doc", id: "cours/r2.2", customProps: { calendrier: {
           "Jean-Michel": [{"1010": "2026-09-25"},
                           {"1020": "2026-09-25"}],
           "Vincent":     [{"1030": "2026-09-29"}],
           "Mathieu":     [{"1040": "2026-09-28"},
                           {"1050": "2026-09-28"}],
-        }, avancementLabel: "TP2", avancement: 0.20 }},
-    { type: "doc", id: "cours/r6.1", customProps: { calendrier: {
+        }, avancementLabel: "TP2", avancement: 0.10 }},
+    { type: "doc", id: "cours/r2.3", customProps: { calendrier: {
           "Jean-Michel": [{"1010": "2026-09-29"},
                           {"1020": "2026-09-29"}],
           "Vincent":     [{"1030": "2026-10-02"}],
           "Mathieu":     [{"1040": "2026-09-30"},
                           {"1050": "2026-09-30"}],
-        }, avancementLabel: "TP2", avancement: 0.30 }},
-    { type: "doc", id: "cours/r6.2", customProps: { calendrier: {
+        }, avancementLabel: "TP2", avancement: 0.20 }},
+    { type: "doc", id: "cours/r2.4", customProps: { calendrier: {
           "Jean-Michel": [{"1010": "2026-10-02"},
                           {"1020": "2026-10-02"}],
           "Vincent":     [{"1030": "2026-10-09"}],
           "Mathieu":     [{"1040": "2026-10-06"},
                           {"1050": "2026-10-06"}],
-        }, avancementLabel: "TP2", avancement: 0.40 }},
-    { type: "doc", id: "cours/r7.1", customProps: { calendrier: {
+        }, avancementLabel: "TP2", avancement: 0.30 }},
+    { type: "doc", id: "cours/r2.5", customProps: { calendrier: {
           "Jean-Michel": [{"1010": "2026-10-09"},
                           {"1020": "2026-10-09"}],
           "Vincent":     [{"1030": "2026-10-20"}],
           "Mathieu":     [{"1040": "2026-10-07"},
                           {"1050": "2026-10-07"}],
-        }, avancementLabel: "TP2", avancement: 0.50 }},
-    { type: "doc", id: "cours/r7.2", customProps: { calendrier: {
+        }, avancementLabel: "TP2", avancement: 0.40 }},
+    { type: "doc", id: "cours/r2.6", customProps: { calendrier: {
           "Jean-Michel": [{"1010": "2026-10-20"},
                           {"1020": "2026-10-20"}],
           "Vincent":     [{"1030": "2026-10-23"}],
           "Mathieu":     [{"1040": "2026-10-19"},
                           {"1050": "2026-10-19"}],
         }, avancementLabel: "TP2", avancement: 0.60 }},
-    { type: "doc", id: "cours/r8.1", customProps: { calendrier: {
+    { type: "doc", id: "cours/r2.7", customProps: { calendrier: {
           "Jean-Michel": [{"1010": "2026-10-23"},
                           {"1020": "2026-10-23"}],
           "Vincent":     [{"1030": "2026-10-27"}],
           "Mathieu":     [{"1040": "2026-10-21"},
                           {"1050": "2026-10-21"}],
         }, avancementLabel: "TP2", avancement: 0.75 }},
-    { type: "doc", id: "cours/r8.2", customProps: { calendrier: {
+    { type: "doc", id: "cours/r2.8", customProps: { calendrier: {
           "Jean-Michel": [{"1010": "2026-10-27"},
                           {"1020": "2026-10-27"}],
           "Vincent":     [{"1030": "2026-10-30"}],
           "Mathieu":     [{"1040": "2026-10-26"},
                           {"1050": "2026-10-26"}],
         }, avancementLabel: "TP2", avancement: 0.90 }},
-    { type: "doc", id: "cours/r9.1", customProps: { calendrier: {
+    { type: "doc", id: "cours/r2.9", customProps: { calendrier: {
           "Jean-Michel": [{"1010": "2026-10-30"},
                           {"1020": "2026-10-30"}],
           "Vincent":     [{"1030": "2026-11-03"}],
           "Mathieu":     [{"1040": "2026-10-28"},
                           {"1050": "2026-10-28"}],
         }, avancementLabel: "TP2", avancement: 1.00 }},
-    { type: "doc", id: "cours/r9.2", customProps: { calendrier: {
+    { type: "doc", id: "cours/r2.10", customProps: { calendrier: {
           "Jean-Michel": [{"1010": "2026-11-03"},
                           {"1020": "2026-11-03"}],
           "Vincent":     [{"1030": "2026-11-06"}],
           "Mathieu":     [{"1040": "2026-11-02"},
                           {"1050": "2026-11-02"}],
         }  }},
-    { type: "doc", id: "cours/r10.1", customProps: { calendrier: {
+    { type: "doc", id: "cours/r2.11", customProps: { calendrier: {
           "Jean-Michel": [{"1010": "2026-11-06"},
                           {"1020": "2026-11-06"}],
           "Vincent":     [{"1030": "2026-11-10"}],
           "Mathieu":     [{"1040": "2026-11-04"},
                           {"1050": "2026-11-04"}],
         }  }},
-    { type: "doc", id: "cours/r10.2", customProps: { calendrier: {
+    { type: "doc", id: "cours/r3.1", customProps: { calendrier: {
           "Jean-Michel": [{"1010": "2026-11-10"},
                           {"1020": "2026-11-10"}],
           "Vincent":     [{"1030": "2026-11-13"}],
           "Mathieu":     [{"1040": "2026-11-09"},
                           {"1050": "2026-11-09"}],
-        }, avancementLabel: "T31", avancement: 0.10 }},
-    { type: "doc", id: "cours/r11.1", customProps: { calendrier: {
+        }, avancementLabel: "TP3", avancement: 0.10 }},
+    { type: "doc", id: "cours/r3.2", customProps: { calendrier: {
           "Jean-Michel": [{"1010": "2026-11-13"},
                           {"1020": "2026-11-13"}],
           "Vincent":     [{"1030": "2026-11-17"}],
           "Mathieu":     [{"1040": "2026-11-16"},
                           {"1050": "2026-11-16"}],
         }, avancementLabel: "TP3", avancement: 0.20 }},
-    { type: "doc", id: "cours/r11.2", customProps: { calendrier: {
+    { type: "doc", id: "cours/r3.3", customProps: { calendrier: {
           "Jean-Michel": [{"1010": "2026-11-17"},
                           {"1020": "2026-11-17"}],
           "Vincent":     [{"1030": "2026-11-20"}],
           "Mathieu":     [{"1040": "2026-11-18"},
                           {"1050": "2026-11-18"}],
         }, avancementLabel: "TP3", avancement: 0.30 }},
-    { type: "doc", id: "cours/r12.1", customProps: { calendrier: {
+    { type: "doc", id: "cours/r3.4", customProps: { calendrier: {
           "Jean-Michel": [{"1010": "2026-11-20"},
                           {"1020": "2026-11-20"}],
           "Vincent":     [{"1030": "2026-11-24"}],
           "Mathieu":     [{"1040": "2026-11-23"},
                           {"1050": "2026-11-23"}],
         }, avancementLabel: "TP3", avancement: 0.40 }},
-    { type: "doc", id: "cours/r12.2", customProps: { calendrier: {
+    { type: "doc", id: "cours/r3.5", customProps: { calendrier: {
           "Jean-Michel": [{"1010": "2026-11-24"},
                           {"1020": "2026-11-24"}],
           "Vincent":   [{"1030": "2026-11-27"}],
           "Mathieu":   [{"1040": "2026-11-25"},
                           {"1050": "2026-11-25"}],
         }, avancementLabel: "TP3", avancement: 0.50 }},
-    { type: "doc", id: "cours/r13.1", customProps: { calendrier: {
+    { type: "doc", id: "cours/r3.6", customProps: { calendrier: {
           "Jean-Michel": [{"1010": "2026-11-27"},
                           {"1020": "2026-11-27"}],
           "Vincent":   [{"1030": "2026-12-01"}],
           "Mathieu":   [{"1040": "2026-11-30"},
                           {"1050": "2026-11-30"}],
         }, avancementLabel: "TP3", avancement: 0.60 }},
-    { type: "doc", id: "cours/r13.2", customProps: { calendrier: {
+    { type: "doc", id: "cours/r3.7", customProps: { calendrier: {
           "Jean-Michel": [{"1010": "2026-12-01"},
                           {"1020": "2026-12-01"}],
           "Vincent":   [{"1030": "2026-12-04"}],
           "Mathieu":   [{"1040": "2026-12-02"},
                           {"1050": "2026-12-02"}],
         }, avancementLabel: "TP3", avancement: 0.70 }},
-    { type: "doc", id: "cours/r14.1", customProps: { calendrier: {
+    { type: "doc", id: "cours/r3.8", customProps: { calendrier: {
           "Jean-Michel": [{"1010": "2026-12-04"},
                           {"1020": "2026-12-04"}],
           "Vincent":     [{"1030": "2026-12-08"}],
           "Mathieu":     [{"1040": "2026-12-07"},
                           {"1050": "2026-12-07"}],
         }, avancementLabel: "TP3", avancement: 0.80 }},
-    { type: "doc", id: "cours/r14.2", customProps: { calendrier: {
+    { type: "doc", id: "cours/r3.9", customProps: { calendrier: {
           "Jean-Michel": [{"1010": "2026-12-08"},
                           {"1020": "2026-12-08"}],
           "Mathieu":     [{"1040": "2026-12-09"},
                           {"1050": "2026-12-09"}],
         }, avancementLabel: "TP3", avancement: 1.00 }},
-    { type: "doc", id: "cours/r15.1", customProps: { calendrier: {
+    { type: "doc", id: "cours/r3.10", customProps: { calendrier: {
           "Jean-Michel": [{"1010": "2026-12-11"},
                           {"1020": "2026-12-11"}],
           "Vincent":     [{"1030": "2026-12-11"}],
           "Mathieu":     [{"1040": "2026-12-14"},
                           {"1050": "2026-12-14"}],
         }  }},
-    { type: "doc", id: "cours/r15.2", customProps: { calendrier: {
+    { type: "doc", id: "cours/r3.11", customProps: { calendrier: {
           "Jean-Michel": [{"1010": "2026-12-22"},
                           {"1020": "2026-12-22"}],
           "Vincent":     [{"1030": "2026-12-22"}],
